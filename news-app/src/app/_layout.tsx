ff14966ab2a624758fc46dbe768722409e822152
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2026-04-20 05:36:28 +0200

    feat: added native bottom tabs

--- news-app/src/app/_layout.tsx
+++ news-app/src/app/_layout.tsx
@@ -1,6 +1,26 @@
+import { useColors } from "@/colors/useColors";
 import "../../global.css";
 import { Stack } from "expo-router";
+import { NativeTabs } from "expo-router/unstable-native-tabs";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colors = useColors();
+
+  return (
+    <NativeTabs
+      backgroundColor={colors.background}
+      indicatorColor={colors.text}
+      labelStyle={{ selected: { color: colors.text } }}
+    >
+      <NativeTabs.Trigger name="index">
+        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="settings">
+        <NativeTabs.Trigger.Label>Settings</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="gear" md="settings" />
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
 }
