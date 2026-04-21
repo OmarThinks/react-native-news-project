aea3111478b1b1c56daa2948922f9ba5df65d3a6
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2026-04-21 11:55:31 +0200

    feat: made it work on web

--- news-app/src/utils/ReactQueryReactNativeSetup.tsx
+++ news-app/src/utils/ReactQueryReactNativeSetup.tsx
@@ -1,23 +1,28 @@
 import { onlineManager } from "@tanstack/react-query";
 import * as Network from "expo-network";
+import { Platform } from "react-native";
 
-onlineManager.setEventListener((setOnline) => {
-  let initialised = false;
+if (Platform.OS === "web") {
+  // Web-specific implementation
+} else {
+  onlineManager.setEventListener((setOnline) => {
+    let initialised = false;
 
-  const eventSubscription = Network.addNetworkStateListener((state) => {
-    initialised = true;
-    setOnline(!!state.isConnected);
-  });
-
-  Network.getNetworkStateAsync()
-    .then((state) => {
-      if (!initialised) {
-        setOnline(!!state.isConnected);
-      }
-    })
-    .catch(() => {
-      // getNetworkStateAsync can reject on some platforms/SDK versions
+    const eventSubscription = Network.addNetworkStateListener((state) => {
+      initialised = true;
+      setOnline(!!state.isConnected);
     });
 
-  return eventSubscription.remove;
-});
+    Network.getNetworkStateAsync()
+      .then((state) => {
+        if (!initialised) {
+          setOnline(!!state.isConnected);
+        }
+      })
+      .catch(() => {
+        // getNetworkStateAsync can reject on some platforms/SDK versions
+      });
+
+    return eventSubscription.remove;
+  });
+}
